eff723e06441272f47e52c56e30c1e8c63fffe6f
Maciej Woś <maciej.wos@gmail.com>
2023-12-21 15:53:29 +0000

    update

--- script/src/main.sw
+++ script/src/main.sw
@@ -1,11 +1,56 @@
 script;
 
+use std::string::String;
+
 struct MyStruct {
     one: u64,
     two: u64,
 }
 
+pub fn read_file_raw(seek: u64, len: u64, path: str) -> raw_slice {
+    let path_ptr = path.as_ptr();
+    let path_len = path.len();
+    let data = (seek, len, path_ptr, path_len);
+    let ptr = __addr_of(data);
+    // ecal 0 for read_file
+    let r_a = 0u64;
+    // unused
+    let r_c = 0u64;
+    let r_d = 0u64;
+    asm(ptr: ptr, r_a: r_a, r_c: r_c, r_d: r_d) {
+        ecal r_a ptr r_c r_d;
+    };
+    let output = (0u64, 0u64);
+    let out_ptr = __addr_of(output);
+    ptr.copy_to::<(u64, u64)>(out_ptr, 1);
+    asm(output: output) {
+        output: raw_slice
+    }
+}
+
+pub fn println(input: String) {
+    let data = (input.as_bytes().buf.ptr(), input.as_bytes().len());
+    let ptr = __addr_of(data);
+    // ecal 1 for println
+    let r_a = 1u64;
+    // unused
+    let r_c = 0u64;
+    let r_d = 0u64;
+    asm(r_a: r_a, r_b: ptr, r_c: r_c, r_d: r_d) {
+        ecal r_a r_b r_c r_d;
+    }
+}
+
+pub fn println_str(input: str) {
+    println(String::from_ascii_str(input))
+}
+
 fn main(value: MyStruct) -> bool {
+    println_str("START");
+    let output = read_file_raw(0, 32, "test_input");
+    let output = String::from(output);
+    println(output);
+    println_str("END");
     let result = value.one + value.two;
     result == 7
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{
 use anyhow::Context;
 
 use fuel_asm::{PanicReason, RegId};
-use fuel_tx::{ConsensusParameters, Finalizable, Script, TransactionBuilder, Receipt};
+use fuel_tx::{ConsensusParameters, Finalizable, Receipt, Script, TransactionBuilder};
 use fuel_vm::{
     error::SimpleResult,
     interpreter::EcalHandler,
@@ -16,67 +16,116 @@ use fuel_vm::{
 };
 
 #[derive(Debug, Clone, Copy, Default)]
-pub struct FileReadEcal;
+pub struct MyEcal;
 
-impl EcalHandler for FileReadEcal {
+type VM = Interpreter<MemoryStorage, Script, MyEcal>;
+
+impl EcalHandler for MyEcal {
     fn ecal<S, Tx>(
         vm: &mut Interpreter<S, Tx, Self>,
-        a: RegId,
-        b: RegId,
-        c: RegId,
-        d: RegId,
+        ra: RegId,
+        rb: RegId,
+        rc: RegId,
+        rd: RegId,
     ) -> SimpleResult<()> {
-        let a = vm.registers()[a]; // Seek offset
-        let b = vm.registers()[b]; // Read length
-        let c = vm.registers()[c]; // File path pointer in vm memory
-        let d = vm.registers()[d]; // File path length
+        let a = vm.registers()[ra];
+        match a {
+            0 => Self::read_file_ecal(vm, ra, rb, rc, rd),
+            1 => Self::println_ecal(vm, ra, rb, rc, rd),
+            _ => panic!("Unexpected ECAL function number {a}"),
+        }
+    }
+}
 
-        vm.gas_charge(b.saturating_add(1))?;
+impl MyEcal {
+    fn read_file_ecal<S, Tx>(
+        vm: &mut Interpreter<S, Tx, Self>,
+        _ra: RegId,
+        rb: RegId,
+        _rc: RegId,
+        _rd: RegId,
+    ) -> SimpleResult<()> {
+        let args: (u64, u64, u64, u64) = {
+            let addr = vm.registers()[rb];
+            let r = MemoryRange::new(addr, 4 * 8)?;
+            let bytes: [u8; 4 * 8] = vm.memory()[r.usizes()].try_into().unwrap();
+            fuels::core::codec::try_from_bytes(&bytes, fuels::core::codec::DecoderConfig::default())
+                .unwrap()
+        };
+        println!("read_file args = {args:?}");
+
+        vm.gas_charge(args.1.saturating_add(1))?;
 
         // Extract file path from vm memory
-        let r = MemoryRange::new(c, d)?;
-        let path = String::from_utf8_lossy(&vm.memory()[r.usizes()]);
-        let path = PathBuf::from(path.as_ref());
-        println!("path: {path:?}");
+        let path = {
+            let r = MemoryRange::new(args.2, args.3)?;
+            let path = String::from_utf8_lossy(&vm.memory()[r.usizes()]);
+            let path = PathBuf::from(path.as_ref());
+            println!("read_file file_path = {path:?}");
+            path
+        };
 
         // Seek file to correct position
         let mut file = File::open(path).map_err(|_| PanicReason::EcalError)?;
         let _ = file
-            .seek(SeekFrom::Start(a))
+            .seek(SeekFrom::Start(args.0))
             .map_err(|_| PanicReason::EcalError)?;
 
         // Allocate the buffer in the vm memory and read directly from the file into it
-        vm.allocate(b)?;
-        let r = MemoryRange::new(vm.registers()[RegId::HP], b)?;
-        file.read(&mut vm.memory_mut()[r.usizes()])
-            .map_err(|_| PanicReason::EcalError)?;
+        let output = {
+            vm.allocate(args.1)?;
+            let r: MemoryRange = MemoryRange::new(vm.registers()[RegId::HP], args.1)?;
+            let len = file
+                .read(&mut vm.memory_mut()[r.usizes()])
+                .map_err(|_| PanicReason::EcalError)?;
+            println!("read_file read {len} bytes");
+            (r.start as u64, len as u64)
+        };
+
+        let output_bytes: Vec<u8> =
+            fuels::core::codec::calldata!(output).expect("Failed to encode output tuple");
+        vm.allocate(output_bytes.len() as u64)?;
+        let o = MemoryRange::new(vm.registers()[RegId::HP], output_bytes.len())?;
+        println!("output = {} {:?}", o.start, o.usizes());
+        vm.memory_mut()[o.usizes()].copy_from_slice(&output_bytes);
+
+        // Return the address of the output tuple through the rB register
+        vm.registers_mut()[rb] = o.start as u64;
 
         Ok(())
     }
+
+    fn println_ecal<S, Tx>(
+        vm: &mut Interpreter<S, Tx, Self>,
+        _ra: RegId,
+        rb: RegId,
+        _rc: RegId,
+        _rd: RegId,
+    ) -> SimpleResult<()> {
+        let str: String = {
+            let addr = vm.registers()[rb];
+            let r = MemoryRange::new(addr, 4 * 8)?;
+            let bytes: [u8; 4 * 8] = vm.memory()[r.usizes()].try_into().unwrap();
+            let (addr, len): (u64, u64) = fuels::core::codec::try_from_bytes(&bytes, fuels::core::codec::DecoderConfig::default())
+                .unwrap();
+            let r = MemoryRange::new(addr, len)?;
+            let bytes = vm.memory()[r.usizes()].to_vec();
+            String::from_utf8(bytes).unwrap()
+        };
+        println!("{str}");
+        Ok(())
+    }
 }
+
+use std::convert::TryInto;
 
 fuels::prelude::abigen!(Script(
     name = "MyScript",
     abi = "script/out/debug/script-abi.json"
 ));
 
-#[allow(unused)]
-async fn run_script_sdk() -> anyhow::Result<()> {
-    let wallet = fuels::prelude::launch_provider_and_get_wallet().await?;
-    let bin_path = "script/out/debug/script.bin";
-    let script_instance = MyScript::new(wallet, bin_path);
-
-    let script_input = MyStruct { one: 3, two: 4 };
-
-    let result = script_instance.main(script_input).call().await?;
-
-    assert_eq!(result.value, true);
-
-    Ok(())
-}
-
-fn run_script_vm() -> Vec<Receipt> {
-    let vm: Interpreter<MemoryStorage, Script, FileReadEcal> = Interpreter::with_memory_storage();
+fn run_script() -> Vec<Receipt> {
+    let vm: VM = Interpreter::with_memory_storage();
 
     let script_path = "script/out/debug/script.bin";
 
@@ -91,10 +140,10 @@ fn run_script_vm() -> Vec<Receipt> {
         contents
     };
 
-    let script_data: Vec<u8> = fuels::core::codec::calldata!(MyStruct { one: 1, two: 6 })
+    let script_data: Vec<u8> = fuels::core::codec::calldata!(MyStruct { one: 1, two: 2 })
         .expect("Failed to encode struct");
 
-    let mut client = MemoryClient::from_txtor(vm.into());
+    let mut client = MemoryClient::from_txtor(vm.clone().into());
     let consensus_params = ConsensusParameters::standard();
     let tx = TransactionBuilder::script(script, script_data)
         .gas_price(0)
@@ -105,17 +154,13 @@ fn run_script_vm() -> Vec<Receipt> {
         .into_checked(Default::default(), &consensus_params)
         .expect("Failed to generate a checked tx");
     client.transact(tx);
-    let receipts = client.receipts().expect("Expected receipts");
-    receipts.to_owned()
+    client.receipts().expect("Expected receipts").to_owned()
 }
 
 #[tokio::main]
 async fn main() {
-    // println!("Running script through SDK");
-    // run_script_sdk().await.unwrap();
-
-    println!("> Running script directly on the VM");
-    let receipts = run_script_vm();
+    println!("> Running script");
+    let receipts = run_script();
     println!("> Receipts");
     println!("{receipts:#?}");
 }
